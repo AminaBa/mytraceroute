efef93d28aebd81f093552e945e660cd3913bb30
4m!na <ea_bali@esi.dz>
2019-10-06 21:49:37 +0200

    Update mytraceroute.py
    Updated Traceroute : perform traceroute showing hops details.

--- mytraceroute.py
+++ mytraceroute.py
@@ -1,3 +1,15 @@
+"""
+Author:
+=======
+Amina BALI
+Master 2 Réseaux 
+Sorbonne Université
+
+Description:
+============
+Perform a traceroute showing hops details : ASN, Network name, Country code and description.
+
+"""
 import argparse
 from ipwhois import IPWhois
 from scapy.layers.inet import IP, UDP
@@ -5,10 +17,10 @@ from scapy.sendrecv import sr1
 import IPy
 #from pprint import pprint as pp
 
-def mytraceroute(destination):
+def mytraceroute(destination, timeout):
 	for i in range(1,31):
 		pkt = IP(dst=destination, ttl=i) / UDP(dport=33434)
-		reply = sr1(pkt, verbose=0, timeout=10)
+		reply = sr1(pkt, verbose=0, timeout=timeout)
 		if reply is not None:
 			ip_addr = reply.src
 			if IPy.IP(ip_addr).iptype() != "PRIVATE":
@@ -30,7 +42,8 @@ def mytraceroute(destination):
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(prog='mytraceroute.py', usage='mytraceroute.py -d <destination_domain>', description='Perform traceroute')
+	parser = argparse.ArgumentParser(prog='mytraceroute.py', usage='mytraceroute.py <destination_domain> -t <timeout_value>', description='Perform traceroute')
 	parser.add_argument('destination')
+	parser.add_argument('-t', '--timeout', help='timeout value in seconds', type=int, default=10)
 	args = parser.parse_args()
-	mytraceroute(args.destination)
+	mytraceroute(args.destination, args.timeout)
